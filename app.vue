cec03a72b6aeea623b3c04642d8a66858ace4368
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-05-11 14:52:23 +0800

    🎨 Icon

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <template>
-  <TheHeader />
+  <Head><Link rel="shortcut icon" :href="favicon" /></Head>
+  <TheHeader class="mb-4" />
   <NuxtPage />
   <TheFooter />
   <ClientOnly>
     <AppRibbon />
   </ClientOnly>
 </template>
+<script setup lang="ts">
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@unocss/reset/tailwind-compat.css';
+import favicon from './assets/favicon.svg';
+</script>
--- pages/rent.vue
+++ pages/rent.vue
@@ -1,1 +1,11 @@
-<template>here is the rent page</template>
+<template>here is the rent page.{{ query }}</template>
+<script setup lang="ts">
+interface Item {
+  id: string;
+  name: string;
+}
+
+const route = useRoute();
+
+const query = computed(() => route.query);
+</script>
