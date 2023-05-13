5e0354b648c57366ad4401232b336876f738481a
Dustella <fdnoaivj@outlook.com>
2023-05-13 18:36:25 +0800

    🔥 yes, KV http API working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+// get a, b and c from env
+// const { a, b, c } = process.env;
+const { accountId, apiToken, namespaceId } = process.env;
+
 export default defineNuxtConfig({
   modules: ['@unocss/nuxt'],
+  nitro: {
+    storage: {
+      KV: {
+        driver: 'cloudflare-kv-http',
+        accountId,
+        apiToken,
+        namespaceId,
+      },
+    },
+  },
 });
---
+++ server/utils/useKV.ts
@@ -0,0 +1,76 @@
+import { ofetch, FetchOptions } from 'ofetch';
+
+const { accountId, apiToken, namespaceId } = process.env;
+
+type KVValue = {
+  name: string;
+  metadata: { [index: string]: string };
+  expiration: number;
+};
+
+type KVResponse<T> = {
+  errors: { ode: number; message: string };
+  messages: [];
+  result: T;
+  success: boolean;
+};
+
+const useKV = () => {
+  // use KV API to store data, this function provides a simple wrapper for calling KV API
+  const apiKV = async <T>(url: string, fetchOptions: FetchOptions<'json'>) => {
+    const baseURL = `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${namespaceId}`;
+
+    const options: FetchOptions<'json'> = {
+      baseURL,
+      headers: {
+        'X-Auth-Key': apiToken!,
+        'X-Auth-Email': 'nuist.kx@outlook.com',
+      },
+      ...fetchOptions,
+    };
+
+    return ofetch<T>(url, options);
+  };
+
+  // https://developers.cloudflare.com/api/operations/workers-kv-namespace-write-key-value-pair-with-metadata
+  const getKeys = async () => {
+    const { result } = await apiKV<KVResponse<KVValue[]>>('/keys', {
+      method: 'GET',
+    });
+    return result;
+  };
+
+  // https://developers.cloudflare.com/api/operations/workers-kv-namespace-write-key-value-pair-with-metadata
+  const setKey = async (
+    key: string,
+    value: string | Buffer,
+    metadata: { [index: string]: string },
+  ) => {
+    // construct form data
+    const body = new FormData();
+    body.append('value', value.toString('hex'));
+    body.append('metadata', JSON.stringify(metadata));
+
+    const { success } = await apiKV<KVResponse<{}>>(`/values/${key}`, {
+      method: 'PUT',
+      body,
+    });
+    return success;
+  };
+
+  // https://developers.cloudflare.com/api/operations/workers-kv-namespace-write-key-value-pair-with-metadata
+  const getKey = async (key: string) => {
+    const data = await apiKV<string>(`/values/${key}`, {
+      method: 'GET',
+    });
+    return data;
+  };
+
+  return {
+    getKeys,
+    setKey,
+    getKey,
+  };
+};
+
+export default useKV;
