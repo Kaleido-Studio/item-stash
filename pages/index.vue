f1f2a85c0f4affe22b940d0a1fc68be0ea4f00a7
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-05-14 16:58:55 +0800

    🚧

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
-const res = await useFetch('/api/hello');
+const file = ref<File | null>(null);
+const imageURL = ref<string | null>(null);
+watch(file, async () => {
+  if (!file.value) {
+    imageURL.value = null;
+    return;
+  }
+  const buffer = await file.value.arrayBuffer();
+  const blob = new Blob([buffer], { type: file.value.type });
+  imageURL.value = URL.createObjectURL(blob);
+});
+const handleInput = (e: Event) => {
+  const inputElement = e.target as HTMLInputElement;
+  if (!inputElement.files) return;
+  [file.value] = inputElement.files;
+};
 </script>
 
 <template>
   <div>
     <h1>Hello There</h1>
-    {{ res }}
+    <img v-if="imageURL" :src="imageURL" height="200" width="200" />
+
+    <label class="hover:cursor-pointer">
+      <input hidden accept="image/*" type="file" @change="handleInput" />
+      上传图片
+    </label>
   </div>
 </template>
