a6e2c911d9613977c920a21f39e8f5fd86e6a356
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-06-07 18:46:22 +0800

    🔒️ Authorize

--- server/api/entries.get.ts
+++ server/api/entries.get.ts
@@ -1,4 +1,24 @@
-export default defineEventHandler(async () => {
+import bcrypt from 'bcryptjs';
+
+bcrypt.setRandomFallback((num: number) => Array.from(crypto.getRandomValues(new Int32Array(num))));
+
+export default defineEventHandler(async (e) => {
+  const { authorization } = e.node.req.headers;
+  if (!authorization) {
+    setResponseStatus(e, 401);
+    return { message: 'Unauthorized' };
+  }
+  const password = authorization.split(' ')[1];
+  if (!password) {
+    setResponseStatus(e, 401);
+    return { message: 'Unauthorized' };
+  }
+  const runtimeCfg = useRuntimeConfig();
+  const success = await bcrypt.compare(password, runtimeCfg.PASSWORD);
+  if (!success) {
+    setResponseStatus(e, 401);
+    return { message: 'Unauthorized' };
+  }
   const kv = useKV();
   const keys = await kv.getKeys();
   return keys as {
--- server/api/entries/[key].get.ts
+++ server/api/entries/[key].get.ts
@@ -1,4 +1,25 @@
+import bcrypt from 'bcryptjs';
+
+bcrypt.setRandomFallback((num: number) => Array.from(crypto.getRandomValues(new Int32Array(num))));
+
 export default defineEventHandler(async (event) => {
+  const { authorization } = event.node.req.headers;
+  if (!authorization) {
+    setResponseStatus(event, 401);
+    return { message: 'Unauthorized' };
+  }
+  const password = authorization.split(' ')[1];
+  if (!password) {
+    setResponseStatus(event, 401);
+    return { message: 'Unauthorized' };
+  }
+  const runtimeCfg = useRuntimeConfig();
+  const success = await bcrypt.compare(password, runtimeCfg.PASSWORD);
+  if (!success) {
+    setResponseStatus(event, 401);
+    return { message: 'Unauthorized' };
+  }
+
   const { key } = event.context.params!;
 
   const kv = useKV();
